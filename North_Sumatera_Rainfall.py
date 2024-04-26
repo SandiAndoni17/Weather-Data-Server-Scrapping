b4d5ca77b215b52d10a3200b7aa5ba7796020d13
sandiandoni17 <sandiandoni17@gmail.com>
2024-04-27 06:15:55 +0700

    tingal aws

--- North_Sumatera_Rainfall.py
+++ North_Sumatera_Rainfall.py
@@ -87,26 +87,6 @@ def download_rainfall_data_aaws(url, stations):
     else:
         return "Gagal mengakses data."
 
-
-# Menghitung tanggal yang diinginkan (yaitu, satu hari sebelum tanggal saat ini)
-tanggal_dinamis = datetime.now() - timedelta(days=1)
-
-# Format tanggal untuk digunakan dalam URL
-tanggal_format_url = tanggal_dinamis.strftime("%d-%m-%Y")
-
-# Membuat URL dinamis
-url_dinamis = f"http://202.90.198.212/logger/ftp/logAAWS-{tanggal_format_url}.txt"
-
-# Stasiun yang data curah hujannya ingin diambil
-stations = ['STA3209', 'sta3032', 'sta3212', 'STS1001']
-
-# Download dan cetak data curah hujan
-rainfall_data_aaws = download_rainfall_data_aaws(url_dinamis, stations)
-
-# Mendapatkan tanggal dan waktu saat ini untuk mencetak bersama dengan data
-waktu_sekarang = datetime.now()
-
-
 def download_rainfall_data_aws(url, stations):
     # Mengirim request ke URL
     response = requests.get(url)
@@ -123,22 +103,6 @@ def download_rainfall_data_aws(url, stations):
         return rainfall_data
     else:
         return "Gagal mengakses data."
-
-
-# Menghitung tanggal yang diinginkan (yaitu, satu hari sebelum tanggal saat ini)
-tanggal_dinamis = datetime.now() - timedelta(days=1)
-
-# Format tanggal untuk digunakan dalam URL
-tanggal_format_url = tanggal_dinamis.strftime("%d-%m-%Y")
-
-# Membuat URL dinamis
-url_dinamis = f"http://202.90.198.212/logger/ftp/logAWS-{tanggal_format_url}.txt"
-
-# Stasiun yang data curah hujannya ingin diambil
-stations = ['STA2068', '160051', '160044']
-
-# Download dan cetak data curah hujan
-rainfall_data_aws = download_rainfall_data_aws(url_dinamis, stations)
 
 def download_rainfall_data_aws2(url, stations):
     # Mengirim request ke URL
@@ -159,25 +123,6 @@ def download_rainfall_data_aws2(url, stations):
         return rainfall_data
     else:
         return "Gagal mengakses data."
-
-
-# Menghitung tanggal yang diinginkan (yaitu, satu hari sebelum tanggal saat ini)
-tanggal_dinamis = datetime.now() - timedelta(days=1)
-
-# Format tanggal untuk digunakan dalam URL
-tanggal_format_url = tanggal_dinamis.strftime("%d-%m-%Y")
-
-# Membuat URL dinamis
-url_dinamis = f"http://202.90.198.212/logger/logfile/logAAWS-{tanggal_format_url}.txt"
-
-# Stasiun yang data curah hujannya ingin diambil
-stations = ['STA2295', 'STW1052']
-
-# Download data curah hujan
-rainfall_data_aws2 = download_rainfall_data_aws2(url_dinamis, stations)
-
-
-from openpyxl.styles import Alignment
 
 def update_excel_sheet(sheet, last_entries, rainfall_data_aaws, rainfall_data_aws, rainfall_data_aws2, station_column_mapping):
     combined_data = {}
@@ -215,22 +160,32 @@ def update_excel_sheet(sheet, last_entries, rainfall_data_aaws, rainfall_data_aw
             else:
                 sheet[column_cell] = data if data != 0 else '-'
 
+def main():
+    # Define stations and URLs
+    stations_aaws = ['STA3209', 'sta3032', 'sta3212', 'STS1001']
+    tanggal_dinamis = datetime.now() - timedelta(days=1)
+    tanggal_format_url = tanggal_dinamis.strftime("%d-%m-%Y")
+    url_aaws = f"http://202.90.198.212/logger/ftp/logAAWS-{tanggal_format_url}.txt"
 
+    stations_aws = ['STA2068', '160051', '160044']
+    url_aws = f"http://202.90.198.212/logger/ftp/logAWS-{tanggal_format_url}.txt"
 
+    stations_aws2 = ['STA2295', 'STW1052']
+    url_aws2 = f"http://202.90.198.212/logger/logfile/logAAWS-{tanggal_format_url}.txt"
 
-def main1():
     station_codes = ['150111', 'STA0259', '150108', '150115', '14032795', '150113', '150114', '150109',
                      '14032793', '150106', '150107', '150112', 'STA0203', 'STA0008', 'STA0009', '150110',
                      'sta0178', '150262', '150259', '150261', '150260', 'STG1014']
-    last_entries = get_rainfall_last_entries(station_codes)
-    rainfall_data = get_rainfall_last_entries(station_codes)
 
-    # Memuat workbook dan mengaktifkan worksheet
-    workbook = load_workbook(
-        r'S:\FILE_RWID\PyCharmProjects\python-fundamental\Weather-Data-Server-Scrapping\Template.xlsx')
+    last_entries = get_rainfall_last_entries(station_codes)
+    rainfall_data_aaws = download_rainfall_data_aaws(url_aaws, stations_aaws)
+    rainfall_data_aws = download_rainfall_data_aws(url_aws, stations_aws)
+    rainfall_data_aws2 = download_rainfall_data_aws2(url_aws2, stations_aws2)
+
+    workbook = load_workbook(r'S:\FILE_RWID\PyCharmProjects\python-fundamental\Weather-Data-Server-Scrapping\Template.xlsx')
     sheet = workbook.active
-    # Menambahkan judul dengan tanggal
-    tanggal_kemarin = (datetime.now() - timedelta(days=1)).strftime('%d-%m-%Y')
+
+    tanggal_kemarin = tanggal_dinamis.strftime('%d-%m-%Y')
     sheet['A1'] = f"Monitoring Data Curah Hujan ARG, AWS, dan AAWS Sumatera Utara per {tanggal_kemarin} Jam 00.00 UTC"
 
     station_column_mapping = {
@@ -240,15 +195,17 @@ def main1():
         'STA0203': 'E16', 'STA0008': 'E17', 'STA0009': 'E18', '150110': 'E19',
         'sta0178': 'E20', '150262': 'E21', '150259': 'E22', '150261': 'E23',
         '150260': 'E24', 'STG1014': 'E25', 'STA3209': 'E26', 'sta3032' : 'E27',
-        'sta3212' : 'E28', 'STS1001':'E29', 'STA2068': 'E30', '160051':'E31',
-        '160044':'E32', 'STA2295':'E33', 'STW1052':'E34'
+        'sta3212' : 'E28', 'STS1001': 'E29', 'STA2068': 'E30', '160051': 'E31',
+        '160044': 'E32', 'STA2295': 'E33', 'STW1052': 'E34'
     }
-    update_excel_sheet(sheet, rainfall_data,  rainfall_data_aaws, rainfall_data_aws,rainfall_data_aws2,
-                       station_column_mapping)
 
-    # Simpan workbook yang telah diubah
+    update_excel_sheet(sheet, last_entries, rainfall_data_aaws, rainfall_data_aws, rainfall_data_aws2, station_column_mapping)
+
     save_path = fr'S:\FILE_RWID\PyCharmProjects\python-fundamental\Weather-Data-Server-Scrapping\Monitoring Hujan Alat Otomatis Tanggal {tanggal_kemarin}.xlsx'
     workbook.save(save_path)
 
+if __name__ == "__main__":
+# Calling main1 to run the script
+    main()
 
-main1()
+
